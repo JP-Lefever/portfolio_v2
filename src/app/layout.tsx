fb98d39d33774dd06e21454e2df9520d4abc5f69
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-05-19 17:36:42 +0200

    feat: add hero header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { titleFont, paragraphFont, paragraphFont2} from "@/components/ui/fonts";
 import "./globals.css";
 import React from "react";
 import NavBar from "@/components/layout/navigation/navBar/NavBar";
+import Header from "@/components/layout/header/Header";
 
 
 export default function RootLayout({
@@ -14,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${paragraphFont.className} ${paragraphFont2.variable} ${titleFont.variable}`}>
         <NavBar/>
+        <Header/>
         <main>
         {children}
         </main>
