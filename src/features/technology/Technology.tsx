baf32df9e7f9faaa0583ac4c5e46dd8b9a85fc69
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2026-02-19 14:46:34 +0100

    feat: add new techno

--- src/features/technology/Technology.tsx
+++ src/features/technology/Technology.tsx
@@ -13,15 +13,15 @@ export default function Technology() {
 
             <h2 className={styles.h2}>{titles.technoPage}</h2>
 
-         <section className = {styles.sectionTech}>
-        {technology.map((tech)=>(
-            <article className={styles.articleTech} key={tech.id}>
-                <Image className={styles.image} src={tech.image} alt={tech.label} width={1024} height={860} />
-                <h2 className={styles.label}>{tech.label}</h2>
-            </article>
+             <section className = {styles.sectionTech}>
+            {technology.map((tech)=>(
+                <article className={styles.articleTech} key={tech.id}>
+                    <Image className={styles.image} src={tech.image} alt={tech.label} width={1024} height={860} />
+                    <h2 className={styles.label}>{tech.label}</h2>
+                </article>
 
-        ))}
-    </section>
+            ))}
+             </section>
         </section>
 
 
