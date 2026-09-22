ce0664cd6abd1653466ac6361bd6c04002137b43
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2026-09-23 00:09:41 +0200

    fix: modify header title

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -17,7 +17,8 @@ export default function Header() {
                 <PixelParticle key={i} />
             ))}
         </div>
-        <h1 className={styles.typing}>{header.title} <span className={styles.sub}>{header.subTitle}</span></h1>
+        <h1 className={styles.typing}>{header.title}
+             <span className={styles.sub}>{header.subTitle}</span></h1>
     </section>
 </header>
 
